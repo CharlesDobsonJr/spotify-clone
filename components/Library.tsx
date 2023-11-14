c53be0a99d0da86b0d48e07870b2abde8ad08496
Charles Dobson <charlesmdobsonjr@gmail.com>
2023-11-14 14:32:52 -0500

    Added icon for the playlist.

--- components/Library.tsx
+++ components/Library.tsx
@@ -1,9 +1,30 @@
 "use client";
 
+import { TbPlaylist } from "react-icons/tb";
+
 const Library = () => {
     return (
-        <div>
-            Library!
+        <div className="flex flex-col">
+            <div
+                className="
+                    flex
+                    items-center
+                    justify-between
+                    px-5
+                    pt-4
+                    "
+                >
+                    <div className="
+                        inline-flex
+                        items-center
+                        gap-x-2
+                        "
+                    >
+                        <TbPlaylist />
+
+                    </div>
+
+            </div>            
         </div>
       );
 }
