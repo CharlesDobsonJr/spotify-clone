dd64cdfaa4aadab4587707ce89e65592de0abe7b
Charles Dobson <charlesmdobsonjr@gmail.com>
2023-11-14 23:32:29 -0500

    Added more elements to home page and playlist area.

--- components/Library.tsx
+++ components/Library.tsx
@@ -1,8 +1,13 @@
 "use client";
 
 import { TbPlaylist } from "react-icons/tb";
+import { AiOutlinePlus } from "react-icons/ai";
 
 const Library = () => {
+    const onClick = () => {
+        // Handle upload later
+    };
+
     return (
         <div className="flex flex-col">
             <div
@@ -14,17 +19,44 @@ const Library = () => {
                     pt-4
                     "
                 >
-                    <div className="
+                    <div
+                     className="
                         inline-flex
                         items-center
                         gap-x-2
                         "
                     >
-                        <TbPlaylist />
-
+                        <TbPlaylist className="text-neutral-400" size={26} />
+                        <p
+                            className="
+                            text-neutral-400
+                            font-medium
+                            text-md
+                            "
+                        >
+                            Your Library
+                        </p>
                     </div>
-
-            </div>            
+                    <AiOutlinePlus
+                        onClick={onClick}
+                        size={20}
+                        className="
+                            text-neutral-400
+                            cursor-pointer
+                            hover:text-white
+                            transition
+                            "
+                         />
+            </div> 
+            <div className="
+                flex
+                flex-col
+                gap-y-2
+                mt-4
+                px-3
+            ">
+              List of Songs!  
+            </div>           
         </div>
       );
 }
