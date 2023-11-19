50c360535f0b958b29116a291d0ef6a6afd54a9e
Charles Dobson <charlesmdobsonjr@gmail.com>
2023-11-19 15:53:47 -0500

    Quick change in the types file, string should be number.

--- providers/ModalProvider.tsx
+++ providers/ModalProvider.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const ModalProvider = () => {
     const [isMounted, setIsMounted] = useState(false);
