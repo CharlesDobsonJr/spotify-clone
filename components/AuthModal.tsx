ed1939b972d669320e1d048e31785b01f39f69fc
Charles Dobson <charlesmdobsonjr@gmail.com>
2023-11-28 15:39:42 -0500

    Added magicLink to authModal.

--- components/AuthModal.tsx
+++ components/AuthModal.tsx
@@ -24,6 +24,7 @@ const AuthModal = () => {
         >
             <Auth
                 theme="dark"
+                magicLink
                 providers={["github"]}
                 supabaseClient={supabaseClient}
                 appearance={{
