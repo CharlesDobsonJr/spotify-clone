0d82d713525fc22ba60480cef03b534825c9baf1
Charles Dobson <charlesmdobsonjr@gmail.com>
2023-12-06 11:13:12 -0500

    Added more styling to the Search text.

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -23,7 +23,9 @@ const Search = async ({ searchParams }: SearchProps) => {
         >
             <Header className="from-bg-neutral-900">
                 <div className="mb-2 flex flex-col gap-y-6">
-
+                    <h1 className="text-white text-3xl font-semibold">
+                        Search
+                    </h1>
                 </div>
             </Header>
         </div>
