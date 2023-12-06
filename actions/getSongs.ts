5e651f0129ca13a4c4624735ac6a6d9a7b1210b8
Charles Dobson <charlesmdobsonjr@gmail.com>
2023-12-05 23:42:46 -0500

    Setting up more of the page content and arrangement.

--- actions/getSongs.ts
+++ actions/getSongs.ts
@@ -1,3 +1,23 @@
-const getSongs = async (): Promise<Song[]> => {
-    
-}
+import { Song } from "@/types";
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+
+const getsongs = async (): Promise<Song[]> => {
+    const supabase = createServerComponentClient({
+        cookies: cookies
+    });
+
+    const { data, error } = await supabase
+        .from('songs')
+        .select('*')
+        .order('created_at', { ascending: false });
+
+        if (error) {
+            console.log(error);
+        }
+
+        return (data as any) || [];
+};
+
+export default getsongs;
+// the instructor had getSongs on his in case this breaks
