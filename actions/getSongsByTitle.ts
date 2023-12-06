69e9f922ea2a8ae53ded5818655697650518af68
Charles Dobson <charlesmdobsonjr@gmail.com>
2023-12-06 08:42:10 -0500

    Added more styling to the side bar but the getSongs folder is an issue.

--- actions/getSongsByTitle.ts
+++ actions/getSongsByTitle.ts
@@ -1,11 +1,17 @@
 import { Song } from "@/types";
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
+import getsongs from "./getSongs";
+import getSongs from "./getSongs";
 
-const getsongs = async (): Promise<Song[]> => {
+const getSongsByTitle = async (title: string): Promise<Song[]> => {
     const supabase = createServerComponentClient({
         cookies: cookies
     });
+
+    if (!title) {
+        const allSongs = await getSongs
+    }
 
     const { data, error } = await supabase
         .from('songs')
@@ -19,5 +25,5 @@ const getsongs = async (): Promise<Song[]> => {
         return (data as any) || [];
 };
 
-export default getSongs;
+export default getsongs;
 // the instructor had getSongs on his in case this breaks
