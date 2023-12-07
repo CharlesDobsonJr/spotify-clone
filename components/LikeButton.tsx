12552ad00cb46bfdc4d48a8ca7acafe690ea5877
Charles Dobson <charlesmdobsonjr@gmail.com>
2023-12-07 01:39:49 -0500

    Set up liked buttons and display for songs before styling.

---
+++ components/LikeButton.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { useSessionContext } from "@supabase/auth-helpers-react";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
+import useAuthModal from "@/hooks/useAuthModal";
+import { useUser } from "@/hooks/useUser";
+
+interface LikeButtonProps {
+    songId: string;
+};
+
+const LikeButton: React.FC<LikeButtonProps> = ({
+    songId
+}) => {
+    const router = useRouter();
+    const { supabaseClient } = useSessionContext();
+
+    const authModal = useAuthModal();
+    const { user } = useUser();
+
+    const [isLiked, setIsLiked] = useState(false);
+
+    return ( 
+        <div>
+            Like Button!
+        </div>
+     );
+}
+ 
+export default LikeButton;
