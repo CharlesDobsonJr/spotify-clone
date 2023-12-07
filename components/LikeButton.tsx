475e6dd5e4b6dcef73f56bcdaa2c515c25a21805
Charles Dobson <charlesmdobsonjr@gmail.com>
2023-12-07 02:12:11 -0500

    Getting many errors on the Liked Button portion, so I am saving my work and will come back to it.

--- components/LikeButton.tsx
+++ components/LikeButton.tsx
@@ -2,7 +2,10 @@
 
 import { useSessionContext } from "@supabase/auth-helpers-react";
 import { useRouter } from "next/navigation";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { AiFillHeart, 
+    AiOutlineHeart } from "react-icons/ai";
+import { toast } from "react-hot-toast";
 
 import useAuthModal from "@/hooks/useAuthModal";
 import { useUser } from "@/hooks/useUser";
@@ -22,10 +25,60 @@ const LikeButton: React.FC<LikeButtonProps> = ({
 
     const [isLiked, setIsLiked] = useState(false);
 
+    useEffect(() => {
+        if (!user?.id) {
+            return;
+        }
+
+        const fetchData = async () => {
+            const { data, error } = await supabaseClient
+                .from('liked_songs')
+                .select('*')
+                .eq('user_id', user.id)
+                .eq('song_id', songId)
+                .single();
+
+            if (!error && data) {
+                setIsLiked(true);
+            }
+        };
+
+        fetchData();
+    }, [songId, supabaseClient, user?.id]);
+
+    const Icon = isLiked ? AiFillHeart : AiOutlineHeart;
+
+    const handleLike = async () => {
+        if (!user) {
+            return authModal.onOpen();
+        }
+
+        if (isLiked) {
+            const { error } = await supabaseClient
+                .from('liked_songs')
+                .delete()
+                .eq('user_id', user.id)
+                .eq('song_id', songId);
+        }
+
+        if (error) {
+            toast.error(error.message);
+        }
+        } else {
+            setIsLiked(false);
+        }
+    }
+
     return ( 
-        <div>
-            Like Button!
-        </div>
+        <button
+            onClick={handleLike}
+            className="
+                hover:opacity-75
+                transition
+            "
+        >
+            <Icon color={isLiked ? '#22c55e' : 'white'} size={25} />
+        </button>
      );
 }
  
