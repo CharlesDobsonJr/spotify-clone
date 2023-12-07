56e055417fea1418610fc4928d976f65f6a8e192
Charles Dobson <charlesmdobsonjr@gmail.com>
2023-12-07 01:03:13 -0500

    Installed query-string as well as wrap up code for SearchInput file.

--- components/SearchInput.tsx
+++ components/SearchInput.tsx
@@ -1,6 +1,28 @@
 "use client";
 
+import qs from "query-string";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+
+import useDebounce from "@/hooks/useDebounce";
+
 const SearchInput = () => {
+    const router = useRouter();
+    const [value, setValue] = useState<string>("");
+    const debouncedValue = useDebounce<string>(value, 500);
+
+    useEffect(() => {
+        const query = {
+            title: debouncedValue,
+        };
+
+        const url = qs.stringifyUrl({
+            url: '/search',
+            query: query
+        });
+
+        router.push(url);
+    }, [debouncedValue, router]);
     return ( 
         <div>Search input!</div>
      );
