a999c4a4ad4efd15c40f9982e9422a895587b428
Charles Dobson <charlesmdobsonjr@gmail.com>
2023-12-08 02:35:32 -0500

    Added more settings and changes to Liked and Search pages.

--- app/liked/page.tsx
+++ app/liked/page.tsx
@@ -1,9 +1,22 @@
+import getLikedSongs from "@/actions/getLikedSongs";
+
 export const revalidate = 0;
 
-const Liked = () => {
+const Liked = async () => {
+    const songs = await getLikedSongs();
+
     return ( 
-        <div>
-            Liked songs!
+        <div
+            className="
+                bg-neutral-900
+                rounded-lg
+                h-full
+                w-full
+                overflow-hidden
+                overflow-y-auto
+            "
+        >
+            
         </div>
      );
 }
