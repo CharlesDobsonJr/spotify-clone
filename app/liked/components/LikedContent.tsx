775c314bfddf272594563a522eb8ee7f15f28540
Charles Dobson <charlesmdobsonjr@gmail.com>
2023-12-08 03:00:38 -0500

    Started development on Liked Content page.

---
+++ app/liked/components/LikedContent.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { Song } from "@/types";
+
+interface LikedContentProps {
+    songs: Song[];
+}
+
+const LikedContent: React.FC<LikedContentProps> = ({
+    songs
+}) => {
+    return ( 
+        <div>
+            LikedContent
+        </div>
+     );
+}
+ 
+export default LikedContent;
