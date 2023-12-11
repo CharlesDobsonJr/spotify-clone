5973d2eb58ef1ff92d16d5e1c170a5d4e901261d
Charles Dobson <charlesmdobsonjr@gmail.com>
2023-12-11 18:37:40 -0500

    Created UI for player and now working on volume UI.

--- components/PlayerContent.tsx
+++ components/PlayerContent.tsx
@@ -2,6 +2,8 @@
 
 import { Song } from "@/types";
 import { BsPauseFill, BsPlayFill } from "react-icons/bs";
+import { AiFillStepBackward, AiFillStepForward } from "react-icons/ai";
+import { HiSpeakerWave, HiSpeakerXMark } from "react-icons/hi2";
 
 import MediaItem from "./MediaItem";
 import LikeButton from "./LikeButton";
@@ -16,6 +18,7 @@ const PlayerContent: React.FC<PlayerContentProps> = ({
     songUrl
 }) => {
     const Icon = true ? BsPauseFill : BsPlayFill;
+    const VolumeIcon = true ? HiSpeakerXMark : HiSpeakerWave;
 
     return ( 
         <div className="grid grid-cols-2 md:grid-cols-3 h-full">
@@ -54,9 +57,66 @@ const PlayerContent: React.FC<PlayerContentProps> = ({
                         cursor-pointer
                     "
                 >
-                    <Icon />
+                    <Icon size={30} className="text-black" />
                 </div>
             </div>
+
+            <div
+                className="
+                    hidden
+                    h-full
+                    md:flex
+                    justify-center
+                    items-center
+                    w-full
+                    max-w-[722px]
+                    gap-x-6
+                "
+            >
+                <AiFillStepBackward
+                    onClick={() => {}}
+                    size={30}
+                    className="
+                        text-neutral-400
+                        cursor-pointer
+                        hover:text-white
+                        transition
+                    "
+                />
+                <div
+                    onClick={() => {}}
+                    className="
+                        flex
+                        item-center
+                        justify-center
+                        h-10
+                        w-10
+                        rounded-full
+                        bg-white
+                        p-1
+                        cursor-pointer
+                    "
+                >
+                    <Icon size={30} className="text-black" />
+                </div>
+                <AiFillStepForward
+                    onClick={() => {}}
+                    size={30}
+                    className="
+                        text-neutral-400
+                        cursor-pointer
+                        hover:text-white
+                        transition
+                    "
+                />
+            </div>
+
+            <div className="hidden md:flex w-full justify-end pr-2">
+                <div className="flex items-center gap-x-2 w-[120px]">
+                    <VolumeIcon />
+                </div>
+            </div>
+
         </div>
      );
 }
