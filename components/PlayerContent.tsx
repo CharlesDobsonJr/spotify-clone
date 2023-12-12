4ea6fd9ec57fa59fe6650466e55d38088d228f7c
Charles Dobson <charlesmdobsonjr@gmail.com>
2023-12-12 02:36:26 -0500

    Wrapped up functionality of player including forward and backward UI.

--- components/PlayerContent.tsx
+++ components/PlayerContent.tsx
@@ -28,6 +28,35 @@ const PlayerContent: React.FC<PlayerContentProps> = ({
     const Icon = isPlaying ? BsPauseFill : BsPlayFill;
     const VolumeIcon = volume === 0 ? HiSpeakerXMark : HiSpeakerWave;
 
+    const onPlayNext = () => {
+        if (player.ids.length === 0) {
+            return;
+        }
+
+        const currentIndex = player.ids.findIndex((id) => id === player.activeId);
+        const nextSong = player.ids[currentIndex + 1];
+
+        if (!nextSong) {
+            return player.setId(player.ids[0]);
+        }
+
+        player.setId(nextSong);
+    }
+
+    const onPlayPrevious = () => {
+        if (player.ids.length === 0) {
+            return;
+        }
+
+        const currentIndex = player.ids.findIndex((id) => id === player.activeId);
+        const previousSong = player.ids[currentIndex - 1];
+
+        if (!previousSong) {
+            return player.setId(player.ids[player.ids.length -1]);
+        }
+
+        player.setId(previousSong);
+    }
     return ( 
         <div className="grid grid-cols-2 md:grid-cols-3 h-full">
             <div className="
@@ -82,7 +111,7 @@ const PlayerContent: React.FC<PlayerContentProps> = ({
                 "
             >
                 <AiFillStepBackward
-                    onClick={() => {}}
+                    onClick={onPlayPrevious}
                     size={30}
                     className="
                         text-neutral-400
@@ -108,7 +137,7 @@ const PlayerContent: React.FC<PlayerContentProps> = ({
                     <Icon size={30} className="text-black" />
                 </div>
                 <AiFillStepForward
-                    onClick={() => {}}
+                    onClick={onPlayNext}
                     size={30}
                     className="
                         text-neutral-400
