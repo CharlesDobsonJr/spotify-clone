900d790ef1369be0b480445cb38855c09698dd65
Charles Dobson <charlesmdobsonjr@gmail.com>
2023-12-16 22:58:58 -0500

    Created and set up libs and stripeClient.

--- libs/stripe.ts
+++ libs/stripe.ts
@@ -1,7 +1,7 @@
 import Stripe from "stripe";
 
 export const stripe = new Stripe (
- process.env.STRIPE_SECRET_KEY,
+ process.env.STRIPE_SECRET_KEY ?? '',
  {
     apiVersion: '2023-10-16',
     appInfo: {
@@ -9,6 +9,6 @@ export const stripe = new Stripe (
         version: '0.1.0'
     }
  }   
-)
+);
 
-// I need to check these dates to ensure I have the most up-to-date information
+// I need to check these dates to ensure I have the most up-to-date information.  The video has 2022-11-15 and version 0.1.0
