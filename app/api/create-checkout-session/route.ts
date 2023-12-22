2f4f96ab50f9bb212b81cf0acde0f2d55e2cb095
Charles Dobson <charlesmdobsonjr@gmail.com>
2023-12-21 20:24:40 -0500

    Set up imports in checkout session file.

--- app/api/create-checkout-session/route.ts
+++ app/api/create-checkout-session/route.ts
@@ -1,1 +1,14 @@
-import {} from 
+import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
+import { headers, cookies } from "next/headers";
+import { NextResponse } from "next/server";
+
+import { stripe } from "@/libs/stripe";
+import { getURL } from "@/libs/helpers";
+import { createOrRetrieveCustomer } from "@/libs/supabaseAdmin";
+
+export async function POST(
+    request: Request
+) {
+    const { price, quantity = 1, metadata = {} } = await request.json();
+    
+}
