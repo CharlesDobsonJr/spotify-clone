39b0be6a13261e79bad79adc268598ee450ce2a9
Charles Dobson <charlesmdobsonjr@gmail.com>
2024-01-01 14:46:30 -0500

    Set up functionality of AccountContent file.

--- app/account/components/AccountContent.tsx
+++ app/account/components/AccountContent.tsx
@@ -1,6 +1,40 @@
 "use client";
 
+import { useRouter } from "next/router";
+import { postData } from "@/libs/helpers";
+import toast from "react-hot-toast";
+
+import useSubscribeModal from "@/hooks/useSubscribeModal";
+import { useUser } from "@/hooks/useUser";
+import { useEffect, useState } from "react";
+
 const AccountContent = () => {
+    const router = useRouter();
+    const subscribeModal = useSubscribeModal();
+    const { isLoading, subscription, user } = useUser();
+
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        if (!isLoading && !user) {
+            router.replace('/');
+        }
+    }, [isLoading, user, router]);
+
+    const redirectToCustomerPortal = async () => {
+        setLoading(true);
+        try {
+            const { url, error } = await postData({
+                url: '/api/create-portal-link'
+            });
+            window.location.assign(url);
+        } catch (error) {
+            if (error) {
+                toast.error((error as Error).message);
+            }
+        }
+        setLoading(false);
+    }
     return ( 
         <div>
             Account Content
@@ -8,4 +42,4 @@ const AccountContent = () => {
      );
 }
  
-export default ;
+export default AccountContent;
